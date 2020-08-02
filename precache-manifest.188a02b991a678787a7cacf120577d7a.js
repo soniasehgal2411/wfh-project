16543bc25e99c3acf3d5a0674475aa3e6f797900
soniasehgal2411 <soniasehgal2411@gmail.com>
2020-08-02 23:01:20 +0530

    Updates

---
+++ precache-manifest.188a02b991a678787a7cacf120577d7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "888e5772cf5ef4f6b4b2b497a578b964",
+    "url": "/wfh-project/index.html"
+  },
+  {
+    "revision": "7ea1386d778ad3d0552d",
+    "url": "/wfh-project/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "5711d359c4ddccf2fb6b",
+    "url": "/wfh-project/static/css/main.6453eedf.chunk.css"
+  },
+  {
+    "revision": "7ea1386d778ad3d0552d",
+    "url": "/wfh-project/static/js/2.2d970d7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wfh-project/static/js/2.2d970d7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5711d359c4ddccf2fb6b",
+    "url": "/wfh-project/static/js/main.c655187d.chunk.js"
+  },
+  {
+    "revision": "1434056aa5718cf0922c",
+    "url": "/wfh-project/static/js/runtime-main.adc42190.js"
+  }
+]);
